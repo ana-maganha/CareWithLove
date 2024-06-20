997d882e7b3aca278520ce3684ecd15f04f9c486
Ana <maganha2528@gmail.com>
2024-06-20 10:15:24 -0300

    incluindo react native paper

---
+++ src/data/DadosPrestador.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        id:"1",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"2",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"3",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"4",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"5",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"6",
+        nome:"Notebook Dell Inspiron X",
+        telefone:"Notebook Dell Inspiron X",
+        formacao:"4399,00",
+        especialidades:"Dell",
+        perido:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    }
+]
