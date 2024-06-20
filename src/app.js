744896ea335051f622e72e83e739c927a89f1ff5
Ana <maganha2528@gmail.com>
2024-06-20 12:34:57 -0300

    incluindo drawer

--- src/app.js
+++ src/app.js
@@ -1,53 +1,50 @@
-// In App.js in a new project
 
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import HomeScreen from "./views/HomeScreen"
-import SobreScreen from "./views/SobreScrenn"
-import ContatoScreen from "./views/ContatoScreen"
-import ProdutoScreen from "./views/ProdutoScreen"
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const Tab = createBottomTabNavigator();
+function AgendamentoScreen() {
+    return(
+        <Text>Feed</Text>
+    )
+}
+
+function ContaScreen() {
+    return(
+        <Text>Feed</Text>
+    )
+}
+
+function HomeContratanteScreen() {
+    return(
+        <Text>Article</Text>
+    )
+}
+
+function LoginScreen() {
+    return(
+        <Text>Article</Text>
+    )
+}
+
+function PerfilContratanteScreen() {
+    return(
+        <Text>Article</Text>
+    )
+}
+
+const Drawer = createDrawerNavigator();
 
 function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName='Home'>
-                <Tab.Screen name="Home" component={HomeScreen}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons
-                        name="home" color={color} size={size} />
-                    )
-                }}
-                />
-                <Tab.Screen name="Sobre" component={SobreScreen} 
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons
-                        name="clipboard-text-multiple-outline" color={color} size={size} />
-                    )
-                }}
-                />
-                <Tab.Screen name="Produtos" component={ProdutoScreen} 
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons
-                        name="cart-variant" color={color} size={size} />
-                    )
-                }}
-                />
-                <Tab.Screen name="Contato" component={ContatoScreen} 
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons
-                        name="phone-log" color={color} size={size} />
-                    )
-                }}
-                />
-            </Tab.Navigator>
+            <Drawer.Navigator>
+                <Drawer.Screen name="Agendamentos" component={AgendamentoScreen} />
+                <Drawer.Screen name="Conta" component={ContaScreen} />
+                <Drawer.Screen name="Home" component={HomeContratanteScreen} />
+                <Drawer.Screen name="Login" component={LoginScreen} />
+                <Drawer.Screen name="Perfil" component={PerfilContratanteScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
